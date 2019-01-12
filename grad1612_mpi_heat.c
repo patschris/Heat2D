810ccf259480c575888200833a5e70796053cb35
Christos Patsouras <chpatsouras@gmail.com>
2019-01-12 19:36:09 +0200

    Replace Send/Recv with Bcast for xs,ys,xe,ye

--- grad1612_mpi_heat.c
+++ grad1612_mpi_heat.c
@@ -40,7 +40,7 @@ void prtdat(int, int, float **, char *);
 
 int main (void) {
     
-   int comm_sz, my_rank, neighBor[4], dims[2], periods[2], rc, xcell, ycell, *xs, *ys, *xe, *ye, i, j, size_total_x, size_total_y;
+   int comm_sz, my_rank, neighBor[4], dims[2], periods[2], *xs, *ys, *xe, *ye, i, j;
    float  **u[2]; /* array for grid */
    MPI_Comm comm2d;
    /* Variables for clock */
@@ -64,15 +64,15 @@ int main (void) {
    MPI_Cart_shift(comm2d, 0, 1, &neighBor[WEST], &neighBor[EAST]);
    /* Find Bottom/South and Upper/North neighBors */
    MPI_Cart_shift(comm2d, 1, 1, &neighBor[SOUTH], &neighBor[NORTH]);
-   printf("I am %d and my neighbors are North=%d, South=%d, East =%d, West=%d\n", my_rank, neighBor[NORTH], neighBor[SOUTH], neighBor[EAST], neighBor[WEST]);
+   //printf("I am %d and my neighbors are North=%d, South=%d, East =%d, West=%d\n", my_rank, neighBor[NORTH], neighBor[SOUTH], neighBor[EAST], neighBor[WEST]);
 
    /* Size of each cell */
-   xcell = NXPROB/GRIDX;
-   ycell = NYPROB/GRIDY;
+   int xcell = NXPROB/GRIDX;
+   int ycell = NYPROB/GRIDY;
 
    /* Size with extra rows and columns */
-   size_total_x = NXPROB + 2*GRIDX;
-   size_total_y = NYPROB + 2*GRIDY;
+   int size_total_x = NXPROB + 2*GRIDX;
+   int size_total_y = NYPROB + 2*GRIDY;
 
    /* Allocate 2D contiguous arrays u[0] and u[1] (3d u) */
    /* Allocate size_total_x rows */
@@ -98,18 +98,17 @@ int main (void) {
     if (my_rank == MASTER) {
       if ((comm_sz > MAXWORKER || comm_sz < MINWORKER) && comm_sz != GRIDX*GRIDY) {
         printf("ERROR: the number of tasks must be between %d and %d.\n Quiting...\n", MINWORKER+1,MAXWORKER+1);
-        MPI_Abort(MPI_COMM_WORLD, rc);
+        MPI_Abort(comm2d, 1);
         exit(1);
       }
       else {
-          printf("Starting with %d processes\nProblem size:%dx%d\nEach process will take: %dx%d\n", comm_sz, NXPROB, NYPROB, xcell, ycell);
+         printf("Starting with %d processes\nProblem size:%dx%d\nEach process will take: %dx%d\n", comm_sz, NXPROB, NYPROB, xcell, ycell);
       }                                                          
       /* This subroutine computes the coordinates xs, xe, ys, ye,  for each cell on the grid, respecting processes topology */
       /* Computation of starting ys,ye on (Ox) standard axis for the first column of global domain */
       for (i=0;i<GRIDX;i++) {
          ys[i] = 1;
-         /* Here, ye(0:(GRIDX-1)) = 2+ycell-1 */
-         ye[i] = ys[i]+ycell-1;
+         ye[i] = ys[i]+ycell-1; /* Here, ye(0:(GRIDX-1)) = 2+ycell-1 */
       }
       /* Computation of ys,ye on (Ox) standard axis for all other cells of global domain */
       for (i=1;i<GRIDY;i++) {
@@ -121,8 +120,7 @@ int main (void) {
       /* Computation of starting xs,xe on (Oy) standard axis for the first row of global domain, Convention x(i,j) with i row and j column */
       for (i=0;i<GRIDY;i++) {
          xs[i*GRIDX] = 1;
-         /* Here, xe(i*GRIDX) = 2+xcell-1 */
-         xe[i*GRIDX] = xs[i*GRIDX]+xcell-1;
+         xe[i*GRIDX] = xs[i*GRIDX]+xcell-1; /* Here, xe(i*GRIDX) = 2+xcell-1 */
       }
       /* Computation of xs,xe on (Oy) standard axis for all other cells of global domain */
       for (i=1;i<=GRIDY;i++) {
@@ -131,36 +129,24 @@ int main (void) {
             xe[(i-1)*GRIDX+j] = xs[(i-1)*GRIDX+j]+xcell-1;
          }
       }
+   }
 
-      for (i=1; i<comm_sz; i++) {
-         MPI_Send(xs, comm_sz, MPI_INT, i, 0, comm2d);
-         MPI_Send(ys, comm_sz, MPI_INT, i, 0, comm2d);
-         MPI_Send(xe, comm_sz, MPI_INT, i, 0, comm2d);
-         MPI_Send(ye, comm_sz, MPI_INT, i, 0, comm2d);
-      }
+   MPI_Bcast(xs, comm_sz, MPI_INT, 0, comm2d);
+   MPI_Bcast(ys, comm_sz, MPI_INT, 0, comm2d);
+   MPI_Bcast(xe, comm_sz, MPI_INT, 0, comm2d);
+   MPI_Bcast(ye, comm_sz, MPI_INT, 0, comm2d);
 
-
-      //inidat(size_total_x, size_total_y, u[0]);
+   if (my_rank == MASTER) {
       for (i = 0; i < size_total_x; i++) { 
          for (j = 0; j < size_total_y; j++) {
             u[0][i][j] = i * (size_total_x - i - 1) * j * (size_total_y - j - 1);
             u[1][i][j] = 0;
          }
       }
-      
       prtdat(size_total_x, size_total_y, u[0], "initial.dat");
-
-      printf("xs: "); for (i=0; i<comm_sz; i++) printf("%d ", xs[i]); printf("\n");
-      printf("ys: "); for (i=0; i<comm_sz; i++) printf("%d ", ys[i]); printf("\n");
-      printf("xe: "); for (i=0; i<comm_sz; i++) printf("%d ", xe[i]); printf("\n");
-      printf("ye: "); for (i=0; i<comm_sz; i++) printf("%d ", xe[i]); printf("\n");
-    }
-    /* worker's code */
-    else {
-      MPI_Recv(xs, MPI_COMM_WORLD, MPI_INT, 0, 0, comm2d, MPI_STATUS_IGNORE);
-      MPI_Recv(ys, MPI_COMM_WORLD, MPI_INT, 0, 0, comm2d, MPI_STATUS_IGNORE);
-      MPI_Recv(xe, MPI_COMM_WORLD, MPI_INT, 0, 0, comm2d, MPI_STATUS_IGNORE);
-      MPI_Recv(ye, MPI_COMM_WORLD, MPI_INT, 0, 0, comm2d, MPI_STATUS_IGNORE);
+   }
+   /* worker's code */
+   else {
       for (i = 0; i < size_total_x; i++) {
          for (j = 0; j < size_total_y; j++) {
             if (i>=xs[my_rank] && i<=xe[my_rank]  && j >=ys[my_rank] && j<=ye[my_rank])
@@ -173,7 +159,8 @@ int main (void) {
       char str[12];
       sprintf(str, "%d.txt", my_rank);
       prtdat(size_total_x, size_total_y, u[0], str);
-    }
+   }
+   MPI_Barrier(comm2d);
    /* Starting time */
    start_time = MPI_Wtime();
 
