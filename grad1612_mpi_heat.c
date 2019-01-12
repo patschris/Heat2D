7eca0711a10d512d972fdbce970ba7c95901a37e
Christos Patsouras <chpatsouras@gmail.com>
2019-01-12 15:55:56 +0200

    Fixed ycell size, fixed u (alloc and init), change update functions, remove other functions

--- grad1612_mpi_heat.c
+++ grad1612_mpi_heat.c
@@ -26,56 +26,53 @@
 
 #define CONVERGENCE     0
 #define INTERVAL	      200		            /* After how many rounds are we checking for convergence */
-#define SENSITIVITY	    0.1		            /* Convergence's sensitivity (EPSILON) */
+#define SENSITIVITY	   0.1		            /* Convergence's sensitivity (EPSILON) */
 
-struct Parms { 
-  float cx;
-  float cy;
-} parms = {0.1, 0.1};
+#define CX              0.1
+#define CY              0.1
 
 /**************************************************************************
 * Prototypes
 ****************************************************************************/
-void update(int, int, int, float *, float *);
-void inidat(int, int, float **);
+void update(int, int, int, float **, float **);
 void prtdat(int, int, float **, char *);
-void processToMap(int *, int *, int *, int *, int, int, int, int);
 /****************************************************************************/
 
 int main (void) {
     
-   int comm_sz, my_rank, neighBor[4], dims[2], periods[2], rc, xcell, ycell, *xs, *ys, *xe, *ye, i, size_total_x, size_total_y;
+   int comm_sz, my_rank, neighBor[4], dims[2], periods[2], rc, xcell, ycell, *xs, *ys, *xe, *ye, i, j, size_total_x, size_total_y;
    float  **u[2]; /* array for grid */
    MPI_Comm comm2d;
    /* Variables for clock */
    double start_time, end_time, elapsed_time;
 
-    /* First, find out my taskid and how many tasks are running */
-    MPI_Init(NULL, NULL);
-    MPI_Comm_size(MPI_COMM_WORLD,&comm_sz);
-    MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
+   /* First, find out my taskid and how many tasks are running */
+   MPI_Init(NULL, NULL);
+   MPI_Comm_size(MPI_COMM_WORLD,&comm_sz);
+   MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
 
-    /* Create 2D cartesian grid */
-    periods[0] = periods[1] = 0;
-    /* Invert (Ox,Oy) classic convention */
-    dims[0] = GRIDX;
-    dims[1] = GRIDY;
-    MPI_Cart_create(MPI_COMM_WORLD, 2 , dims, periods, REORGANISATION, &comm2d);
+   /* Create 2D cartesian grid */
+   periods[0] = periods[1] = 0;
+   /* Invert (Ox,Oy) classic convention */
+   dims[0] = GRIDX;
+   dims[1] = GRIDY;
+   MPI_Cart_create(MPI_COMM_WORLD, 2 , dims, periods, REORGANISATION, &comm2d);
     
-    /* Identify neighBors */
-    neighBor[SOUTH] = neighBor[EAST] = neighBor[NORTH] = neighBor[WEST] = MPI_PROC_NULL;
-    /* Find Left/West and Right/East neighBors */
-    MPI_Cart_shift(comm2d, 0, 1, &neighBor[WEST], &neighBor[EAST]);
-    /* Find Bottom/South and Upper/North neighBors */
-    MPI_Cart_shift(comm2d, 1, 1, &neighBor[SOUTH], &neighBor[NORTH]);
+   /* Identify neighBors */
+   neighBor[SOUTH] = neighBor[EAST] = neighBor[NORTH] = neighBor[WEST] = MPI_PROC_NULL;
+   /* Find Left/West and Right/East neighBors */
+   MPI_Cart_shift(comm2d, 0, 1, &neighBor[WEST], &neighBor[EAST]);
+   /* Find Bottom/South and Upper/North neighBors */
+   MPI_Cart_shift(comm2d, 1, 1, &neighBor[SOUTH], &neighBor[NORTH]);
+   printf("I am %d and my neighbors are North=%d, South=%d, East =%d, West=%d\n", my_rank, neighBor[NORTH], neighBor[SOUTH], neighBor[EAST], neighBor[WEST]);
 
-    /* Size of each cell */
-    xcell = NXPROB/GRIDX;
-    ycell = NYPROB/GRIDX;
+   /* Size of each cell */
+   xcell = NXPROB/GRIDX;
+   ycell = NYPROB/GRIDY;
 
    /* Size with extra rows and columns */
-    size_total_x = NXPROB + 2*GRIDX;
-    size_total_y = NYPROB + 2*GRIDY;
+   size_total_x = NXPROB + 2*GRIDX;
+   size_total_y = NYPROB + 2*GRIDY;
 
    /* Allocate 2D contiguous arrays u[0] and u[1] (3d u) */
    /* Allocate size_total_x rows */
@@ -88,7 +85,7 @@ int main (void) {
    for (i=1;i<size_total_x;i++) {
       /* Increment size_total_x block on u[0][i] and u[1][i] address */
       u[0][i] = u[0][0] + i*size_total_y;
-      u[1][i] = u[0][0] + i*size_total_y;
+      u[1][i] = u[1][0] + i*size_total_y;
    }
 
     /* Allocate coordinates of processes */
@@ -97,6 +94,7 @@ int main (void) {
     ys = malloc(comm_sz*sizeof(int));
     ye = malloc(comm_sz*sizeof(int));
 
+   /* master's code */
     if (my_rank == MASTER) {
       if ((comm_sz > MAXWORKER || comm_sz < MINWORKER) && comm_sz != GRIDX*GRIDY) {
         printf("ERROR: the number of tasks must be between %d and %d.\n Quiting...\n", MINWORKER+1,MAXWORKER+1);
@@ -105,15 +103,76 @@ int main (void) {
       }
       else {
           printf("Starting with %d processes\nProblem size:%dx%d\nEach process will take: %dx%d\n", comm_sz, NXPROB, NYPROB, xcell, ycell);
+      }                                                          
+      /* This subroutine computes the coordinates xs, xe, ys, ye,  for each cell on the grid, respecting processes topology */
+      /* Computation of starting ys,ye on (Ox) standard axis for the first column of global domain */
+      for (i=0;i<GRIDX;i++) {
+         ys[i] = 1;
+         /* Here, ye(0:(GRIDX-1)) = 2+ycell-1 */
+         ye[i] = ys[i]+ycell-1;
       }
-      inidat(size_total_x, size_total_y, u[0]);
+      /* Computation of ys,ye on (Ox) standard axis for all other cells of global domain */
+      for (i=1;i<GRIDY;i++) {
+         for (j=0;j<GRIDX;j++) {
+            ys[i*GRIDX+j] = ys[(i-1)*GRIDX+j]+ycell+2;
+            ye[i*GRIDX+j] = ys[i*GRIDX+j]+ycell-1;
+         }
+      }
+      /* Computation of starting xs,xe on (Oy) standard axis for the first row of global domain, Convention x(i,j) with i row and j column */
+      for (i=0;i<GRIDY;i++) {
+         xs[i*GRIDX] = 1;
+         /* Here, xe(i*GRIDX) = 2+xcell-1 */
+         xe[i*GRIDX] = xs[i*GRIDX]+xcell-1;
+      }
+      /* Computation of xs,xe on (Oy) standard axis for all other cells of global domain */
+      for (i=1;i<=GRIDY;i++) {
+         for (j=1;j<GRIDX;j++) {
+            xs[(i-1)*GRIDX+j] = xs[(i-1)*GRIDX+(j-1)]+xcell+2;
+            xe[(i-1)*GRIDX+j] = xs[(i-1)*GRIDX+j]+xcell-1;
+         }
+      }
+
+      for (i=1; i<comm_sz; i++) {
+         MPI_Send(xs, comm_sz, MPI_INT, i, 0, comm2d);
+         MPI_Send(ys, comm_sz, MPI_INT, i, 0, comm2d);
+         MPI_Send(xe, comm_sz, MPI_INT, i, 0, comm2d);
+         MPI_Send(ye, comm_sz, MPI_INT, i, 0, comm2d);
+      }
+
+
+      //inidat(size_total_x, size_total_y, u[0]);
+      for (i = 0; i < size_total_x; i++) { 
+         for (j = 0; j < size_total_y; j++) {
+            u[0][i][j] = i * (size_total_x - i - 1) * j * (size_total_y - j - 1);
+            u[1][i][j] = 0;
+         }
+      }
+      
       prtdat(size_total_x, size_total_y, u[0], "initial.dat");
-   
-      processToMap(xs, ys, xe, ye, xcell, ycell, GRIDX, GRIDY);
+
       printf("xs: "); for (i=0; i<comm_sz; i++) printf("%d ", xs[i]); printf("\n");
       printf("ys: "); for (i=0; i<comm_sz; i++) printf("%d ", ys[i]); printf("\n");
       printf("xe: "); for (i=0; i<comm_sz; i++) printf("%d ", xe[i]); printf("\n");
       printf("ye: "); for (i=0; i<comm_sz; i++) printf("%d ", xe[i]); printf("\n");
+    }
+    /* worker's code */
+    else {
+      MPI_Recv(xs, MPI_COMM_WORLD, MPI_INT, 0, 0, comm2d, MPI_STATUS_IGNORE);
+      MPI_Recv(ys, MPI_COMM_WORLD, MPI_INT, 0, 0, comm2d, MPI_STATUS_IGNORE);
+      MPI_Recv(xe, MPI_COMM_WORLD, MPI_INT, 0, 0, comm2d, MPI_STATUS_IGNORE);
+      MPI_Recv(ye, MPI_COMM_WORLD, MPI_INT, 0, 0, comm2d, MPI_STATUS_IGNORE);
+      for (i = 0; i < size_total_x; i++) {
+         for (j = 0; j < size_total_y; j++) {
+            if (i>=xs[my_rank] && i<=xe[my_rank]  && j >=ys[my_rank] && j<=ye[my_rank])
+               u[0][i][j] = i * (size_total_x - i - 1) * j * (size_total_y - j - 1);
+            else
+               u[0][i][j] = 0.0;
+            u[1][i][j] = 0.0;
+         }
+      }
+      char str[12];
+      sprintf(str, "%d.txt", my_rank);
+      prtdat(size_total_x, size_total_y, u[0], str);
     }
    /* Starting time */
    start_time = MPI_Wtime();
@@ -144,84 +203,22 @@ int main (void) {
 /**************************************************************************
  *  subroutine update
  ****************************************************************************/
-void update(int start, int end, int ny, float *u1, float *u2) {
-   for (int ix = start; ix <= end; ix++) 
-      for (int iy = 1; iy <= ny-2; iy++) 
-         *(u2+ix*ny+iy) = *(u1+ix*ny+iy) + parms.cx * (*(u1+(ix+1)*ny+iy) + *(u1+(ix-1)*ny+iy) - 
-                          2.0 * *(u1+ix*ny+iy)) + parms.cy * (*(u1+ix*ny+iy+1) + *(u1+ix*ny+iy-1) - 2.0 * *(u1+ix*ny+iy));
-}
-
-/*****************************************************************************
- *  subroutine inidat
- *****************************************************************************/
-void inidat(int nx, int ny, float **u) {
-   int ix, iy;
-
-   for (ix = 0; ix < nx; ix++) 
-      for (iy = 0; iy < ny; iy++)
-         u[ix][iy] = ix * (nx - ix - 1) * iy * (ny - iy - 1);
+void update(int start, int end, int ny, float **uold, float **unew) {
+   for (int i = start; i <= end; i++) 
+      for (int j = 1; j <= ny-2; j++)
+         unew[i][j] = uold[i][j] + CX * (uold[i+1][j] + uold[i-1][j]  - 2.0 * uold[i][j]) + CY * (uold[i][j+1] + uold[i][j-1] - 2.0 * uold[i][j]);
 }
 
 /**************************************************************************
  * subroutine prtdat
  **************************************************************************/
 void prtdat(int nx, int ny, float **u, char *fnam) {
-  FILE *fp = fopen(fnam, "w");
-  
-   for (int ix = 0; ix < nx; ix++) {
-      for (int iy = 0; iy < ny; iy++) {
-         fprintf(fp, "%6.1f ", u[ix][iy]);
+   FILE *fp = fopen(fnam, "w");
+   for (int i = 0; i < nx; i++) {
+      for (int j = 0; j < ny; j++) {
+         fprintf(fp, "%6.1f ", u[i][j]);
       }
       fprintf(fp, "\n");
    }
    fclose(fp);
-}
-
-
-/************************************************************/
-/*                                                          */
-/* This subroutine computes the coordinates xs, xe, ys, ye, */
-/* for each cell on the grid, respecting processes topology */
-/*                                                          */
-/************************************************************/
-
-void processToMap(int *xs, int *ys, int *xe, int *ye, int xcell, int ycell, int x_domains, int y_domains) {
-
-   /* Index variables */
-   int i, j;
-
-   /* Computation of starting ys,ye on (Ox) standard axis
-      for the first column of global domain,
-      Convention x(i,j) with i row and j column */
-   for (i=0;i<x_domains;i++) {
-      ys[i] = 1;
-      /* Here, ye(0:(x_domains-1)) = 2+ycell-1 */
-      ye[i] = ys[i]+ycell-1;
-   }
-
-   /* Computation of ys,ye on (Ox) standard axis
-      for all other cells of global domain */
-   for (i=1;i<y_domains;i++)
-      for (j=0;j<x_domains;j++) {
-         ys[i*x_domains+j] = ys[(i-1)*x_domains+j]+ycell+2;
-         ye[i*x_domains+j] = ys[i*x_domains+j]+ycell-1;
-      }
-
-   /* Computation of starting xs,xe on (Oy) standard axis
-      for the first row of global domain,
-      Convention x(i,j) with i row and j column */
-   for (i=0;i<y_domains;i++) {
-      xs[i*x_domains] = 1;
-      /* Here, xe(i*x_domains) = 2+xcell-1 */
-      xe[i*x_domains] = xs[i*x_domains]+xcell-1;
-   }
-
-   /* Computation of xs,xe on (Oy) standard axis
-      for all other cells of global domain */
-   for (i=1;i<=y_domains;i++) {
-      for (j=1;j<x_domains;j++) {
-         xs[(i-1)*x_domains+j] = xs[(i-1)*x_domains+(j-1)]+xcell+2;
-         xe[(i-1)*x_domains+j] = xs[(i-1)*x_domains+j]+xcell-1;
-      }
-   }
 }
