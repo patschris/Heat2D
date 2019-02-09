5bc3eb13d28f128db3af8407ecbfb9981bc8496b
Christos Patsouras <chpatsouras@gmail.com>
2019-02-09 16:34:43 +0200

    Change size of array u, change u initialization, remove persistent communication (MAJOR MAJOR CHANGES)

--- grad1612_hybrid_heat.c
+++ grad1612_hybrid_heat.c
@@ -30,7 +30,6 @@ int main(void) {
    int i, j, k, iz, *xs, *ys, comm_sz, my_rank, neighBor[4], dims[2], periods[2];
    MPI_Comm comm2d;
    MPI_Datatype column, row;
-   MPI_Status status[4];
    MPI_Request recvRequest[4], sendRequest[4];
    /* Variables for clock */
    double start_time, end_time, local_elapsed_time, elapsed_time;
@@ -60,38 +59,37 @@ int main(void) {
    int ycell = NYPROB / GRIDY;
 
    /* Size with extra rows and columns */
-   int size_total_x = NXPROB + 2*GRIDX;
-   int size_total_y = NYPROB + 2*GRIDY;
+   int block_x = xcell + 2;
+   int block_y = ycell + 2;
 
    /* Allocate 2D contiguous arrays u[0] and u[1] (3d u) */
-   /* Allocate size_total_x rows */
-   if ((u[0] = malloc(size_total_x * sizeof(*u[0]))) == NULL) {
+   /* Allocate block_x rows */
+   if ((u[0] = malloc(block_x * sizeof(*u[0]))) == NULL) {
       perror ("u[0] malloc failed");
       MPI_Abort(MPI_COMM_WORLD, 1);
       exit(1);
    }
-   if ((u[1] = malloc(size_total_x * sizeof(*u[1]))) == NULL) {
+   if ((u[1] = malloc(block_x * sizeof(*u[1]))) == NULL) {
       perror ("u[1] malloc failed");
       MPI_Abort(MPI_COMM_WORLD, 1);
       exit(1);
    }
    /* Allocate u[0][0] and u[1][0] for contiguous arrays */
-   if ((u[0][0] = malloc(size_total_x * size_total_y * sizeof(**u[0]))) == NULL) {
+   if ((u[0][0] = malloc(block_x * block_y * sizeof(**u[0]))) == NULL) {
       perror ("u[0][0] malloc failed");
       MPI_Abort(MPI_COMM_WORLD, 1);
       exit(1);
    }
-   if ((u[1][0] = malloc(size_total_x * size_total_y * sizeof(**u[1]))) == NULL) {
+   if ((u[1][0] = malloc(block_x * block_y * sizeof(**u[1]))) == NULL) {
       perror ("u[1][0] malloc failed");
       MPI_Abort(MPI_COMM_WORLD, 1);
       exit(1);
    }
    /* Loop on rows */
-   #pragma omp parallel for num_threads(NUMTHREADS) schedule (static,1) default(none) private(i) shared(u, size_total_x, size_total_y)
-   for (i = 1; i < size_total_x; i++) {
-      /* Increment size_total_x block on u[0][i] and u[1][i] address */
-      u[0][i] = u[0][0] + i * size_total_y;
-      u[1][i] = u[1][0] + i * size_total_y;
+   for (i = 1; i < block_x; i++) {
+      /* Increment block_x block on u[0][i] and u[1][i] address */
+      u[0][i] = u[0][0] + i * block_y;
+      u[1][i] = u[1][0] + i * block_y;
    }
 
    /* Allocate coordinates of processes */
@@ -123,41 +121,52 @@ int main(void) {
 
       /* Compute the coordinates of the top left cell of the array that takes each worker */
       #pragma omp parallel for num_threads(NUMTHREADS) schedule (static,1) default(none) private(i) shared(ys)
-      for (i = 0; i < GRIDX; i++) ys[i] = 1;
-
+      for (i = 0; i < GRIDX; i++) ys[i] = 0;
+      
       #pragma omp parallel for num_threads(NUMTHREADS) collapse(2) schedule (static,1) default(none) private(i,j) shared(ys, ycell)
       for (i = 1; i < GRIDY; i++)
          for (j = 0; j < GRIDX; j++)
-            ys[i * GRIDX + j] = ys[(i - 1) * GRIDX + j] + ycell + 2;
-
-      #pragma omp parallel for num_threads(NUMTHREADS) schedule (static,1) default(none) private(i) shared(xs)
-      for (i = 0; i < GRIDY; i++) xs[i * GRIDX] = 1;
+            ys[i * GRIDX + j] = ys[(i - 1) * GRIDX + j] + ycell;
       
-      #pragma omp parallel for num_threads(NUMTHREADS) collapse(2) schedule (static,1) default(none) private(i,j) shared(xs, xcell) 
+      #pragma omp parallel for num_threads(NUMTHREADS) schedule (static,1) default(none) private(i) shared(xs)
+      for (i = 0; i < GRIDY; i++) xs[i * GRIDX] = 0;
+      
+      #pragma omp parallel for num_threads(NUMTHREADS) collapse(2) schedule (static,1) default(none) private(i,j) shared(xs, xcell)       
       for (i = 1; i <= GRIDY; i++)
          for (j = 1; j < GRIDX; j++)
-            xs[(i - 1) * GRIDX + j] = xs[(i - 1) * GRIDX + (j - 1)] + xcell + 2;
+            xs[(i - 1) * GRIDX + j] = xs[(i - 1) * GRIDX + (j - 1)] + xcell;
    }
 
    /* Create row data type to communicate with North and South neighbors */
    MPI_Type_contiguous(ycell, MPI_FLOAT, &row);
    MPI_Type_commit(&row);
    /* Create column data type to communicate with East and West neighbors */
-   MPI_Type_vector(xcell, 1, size_total_y, MPI_FLOAT, &column);
+   MPI_Type_vector(xcell, 1, block_y, MPI_FLOAT, &column);
    MPI_Type_commit(&column);
 
    MPI_Bcast(xs, comm_sz, MPI_INT, 0, comm2d);
    MPI_Bcast(ys, comm_sz, MPI_INT, 0, comm2d);
 
    /* Each process initializes it's 2D subarray */
-   #pragma omp parallel for num_threads(NUMTHREADS) collapse(2) schedule (static,1) default(none) private(i,j) shared(u, xs, ys, xcell, ycell, my_rank, size_total_x, size_total_y) 
-   for (i = 0; i < size_total_x; i++) {
-      for (j = 0; j < size_total_y; j++) {
-         if (i >= xs[my_rank] && i < xs[my_rank]+xcell && j >= ys[my_rank] && j < ys[my_rank]+ycell)
-            u[0][i][j] = i * (size_total_x - i - 1) * j * (size_total_y - j - 1);
-         else
-            u[0][i][j] = 0.0;
-         u[1][i][j] = 0.0;
+   #pragma omp parallel for num_threads(NUMTHREADS) schedule (static,1) default(none) private(j) shared(u, block_x, block_y) 
+   for (j=0; j<block_y; j++) {
+      u[0][0][j]=0;
+      u[0][block_x-1][j]=0;
+      u[1][0][j]=0;
+      u[1][block_x-1][j]=0;
+   }
+   #pragma omp parallel for num_threads(NUMTHREADS) schedule (static,1) default(none) private(i) shared(u, block_x, block_y) 
+   for (i=0; i<block_x; i++) {
+      u[0][i][0]=0;
+      u[0][i][block_y-1]=0;
+      u[1][i][0]=0;
+      u[1][i][block_y-1]=0;
+   }
+   #pragma omp parallel for num_threads(NUMTHREADS) collapse(2) schedule (static,1) default(none) private(i,j) shared(u, xs, ys, xcell, ycell, my_rank) 
+   for (i = 1; i < xcell+1; i++) {
+      for (j = 1; j < ycell+1; j++) {
+         u[0][i][j] = (i-1+xs[my_rank]) * (NXPROB - i - xs[my_rank]) * (j-1+ys[my_rank]) * (NYPROB - j - ys[my_rank]);
+         u[1][i][j] = 0;
       }
    }
 
@@ -172,66 +181,59 @@ int main(void) {
    start_time = MPI_Wtime();
 
    iz = 0;
-   
-   /* Persistent communication */
-   MPI_Send_init(&u[iz][xs[my_rank]+xcell-1][ys[my_rank]], 1, row, neighBor[SOUTH], 1, comm2d, &sendRequest[SOUTH]); //send a row to south
-   MPI_Send_init(&u[iz][xs[my_rank]][ys[my_rank]], 1, row, neighBor[NORTH], 2, comm2d, &sendRequest[NORTH]); // send a row to north
-   MPI_Send_init(&u[iz][xs[my_rank]][ys[my_rank]+ycell-1], 1, column, neighBor[EAST], 3, comm2d, &sendRequest[EAST]); // send a column to east
-   MPI_Send_init(&u[iz][xs[my_rank]][ys[my_rank]], 1, column, neighBor[WEST], 4, comm2d, &sendRequest[WEST]); // send a column to west
-   
-   MPI_Recv_init(&u[iz][xs[my_rank]-1][ys[my_rank]], 1, row, neighBor[NORTH], 1, comm2d, &recvRequest[NORTH]); // receive a row from north
-   MPI_Recv_init(&u[iz][xs[my_rank]+xcell][ys[my_rank]], 1, row, neighBor[SOUTH], 2, comm2d, &recvRequest[SOUTH]); //receive a row from south
-   MPI_Recv_init(&u[iz][xs[my_rank]][ys[my_rank]-1], 1, column, neighBor[WEST], 3, comm2d, &recvRequest[WEST]); //receive a column from west
-   MPI_Recv_init(&u[iz][xs[my_rank]][ys[my_rank]+ycell], 1, column, neighBor[EAST], 4, comm2d, &recvRequest[EAST]); // receive a column from east
 
    #pragma omp parallel num_threads(NUMTHREADS) private(k)
    for (k = 0; k < STEPS; k++) {
       #pragma omp master
       {
-         /* Receive */
-         MPI_Startall(4, sendRequest);
-         /* Send */
-         MPI_Startall(4, recvRequest);
+         /* Receives */
+         MPI_Irecv(&u[iz][0][1], 1, row, neighBor[NORTH], 1, comm2d, &recvRequest[NORTH]); // receive a row from north
+         MPI_Irecv(&u[iz][xcell+1][1], 1, row, neighBor[SOUTH], 2, comm2d, &recvRequest[SOUTH]); //receive a row from south
+         MPI_Irecv(&u[iz][1][0], 1, column, neighBor[WEST], 3, comm2d, &recvRequest[WEST]); //receive a column from west
+         MPI_Irecv(&u[iz][1][ycell+1], 1, column, neighBor[EAST], 4, comm2d, &recvRequest[EAST]); // receive a column from east
+         /* Sends */
+         MPI_Isend(&u[iz][xcell][1], 1, row, neighBor[SOUTH], 1, comm2d, &sendRequest[SOUTH]); //send a row to south
+         MPI_Isend(&u[iz][1][1], 1, row, neighBor[NORTH], 2, comm2d, &sendRequest[NORTH]); // send a row to north
+         MPI_Isend(&u[iz][1][ycell], 1, column, neighBor[EAST], 3, comm2d, &sendRequest[EAST]); // send a column to east
+         MPI_Isend(&u[iz][1][1], 1, column, neighBor[WEST], 4, comm2d, &sendRequest[WEST]); // send a column to west
       }
-            
+
       /* Update inner elements */
       #pragma omp for schedule(static,1) collapse(2)
-      for (i = xs[my_rank]+1; i < xs[my_rank]+xcell-1; i++)
-         for (j = ys[my_rank]+1; j < ys[my_rank]+ycell-1; j++)
+      for (i = 2; i < xcell; i++)
+         for (j = 2; j < ycell; j++)
             u[1-iz][i][j] = u[iz][i][j] + CX*(u[iz][i+1][j] + u[iz][i-1][j] - 2.0*u[iz][i][j]) + CY*(u[iz][i][j+1] + u[iz][i][j-1] - 2.0*u[iz][i][j]);
-      
-      
-      #pragma omp master
-      MPI_Waitall(4, recvRequest, status); // wait to receive everything
-      
 
-      #pragma omp barrier                 // wait the master who waits
+      #pragma omp master
+      MPI_Waitall(4, recvRequest, MPI_STATUSES_IGNORE);  // wait to receive everything
+      
+      #pragma omp barrier                                // wait the master who waits
+
 
       /* Update boundary elements */
 
       /* First and last row update */
       #pragma omp for schedule(static,1)
-      for (j=ys[my_rank]; j<ys[my_rank]+ycell; j++) {
-         u[1-iz][xs[my_rank]][j] = u[iz][xs[my_rank]][j] + CX*(u[iz][xs[my_rank]+1][j] + u[iz][xs[my_rank]-1][j] - 2.0*u[iz][xs[my_rank]][j]) + CY*(u[iz][xs[my_rank]][j+1] + u[iz][xs[my_rank]][j-1] - 2.0*u[iz][xs[my_rank]][j]);
-         u[1-iz][xs[my_rank]+xcell-1][j] = u[iz][xs[my_rank]+xcell-1][j] + CX*(u[iz][xs[my_rank]+xcell][j] + u[iz][xs[my_rank]+xcell-2][j] - 2.0*u[iz][xs[my_rank]+xcell-1][j]) + CY*(u[iz][xs[my_rank]+xcell-1][j+1] + u[iz][xs[my_rank]+xcell-1][j-1] - 2.0*u[iz][xs[my_rank]+xcell-1][j]);
+      for (j=1; j<ycell+1; j++) {
+         u[1-iz][1][j] = u[iz][1][j] + CX*(u[iz][2][j] + u[iz][0][j] - 2.0*u[iz][1][j]) + CY*(u[iz][1][j+1] + u[iz][1][j-1] - 2.0*u[iz][1][j]);
+         u[1-iz][xcell][j] = u[iz][xcell][j] + CX*(u[iz][xcell+1][j] + u[iz][xcell-1][j] - 2.0*u[iz][xcell][j]) + CY*(u[iz][xcell][j+1] + u[iz][xcell][j-1] - 2.0*u[iz][xcell][j]);
       }
       
       /* First and last column update */
-      #pragma omp for schedule(static,1)
-      for (j=xs[my_rank]+1; j<xs[my_rank]+xcell-1; j++) {
-         u[1-iz][j][ys[my_rank]] = u[iz][j][ys[my_rank]] + CX*(u[iz][j+1][ys[my_rank]] + u[iz][j-1][ys[my_rank]] - 2.0*u[iz][j][ys[my_rank]]) + CY*(u[iz][j][ys[my_rank]+1] + u[iz][j][ys[my_rank]-1] - 2.0*u[iz][j][ys[my_rank]]);
-         u[1-iz][j][ys[my_rank]+ycell-1] = u[iz][j][ys[my_rank]+ycell-1] + CX*(u[iz][j+1][ys[my_rank]+ycell-1] + u[iz][j-1][ys[my_rank]+ycell-1] - 2.0*u[iz][j][ys[my_rank]+ycell-1]) + CY*(u[iz][j][ys[my_rank]+ycell] + u[iz][j][ys[my_rank]+ycell-2] - 2.0*u[iz][j][ys[my_rank]+ycell-1]);
+      #pragma omp for schedule(static,1) 
+      for (j=2; j<xcell; j++) {
+         u[1-iz][j][1] = u[iz][j][1] + CX*(u[iz][j+1][1] + u[iz][j-1][1] - 2.0*u[iz][j][1]) + CY*(u[iz][j][2] + u[iz][j][0] - 2.0*u[iz][j][1]);
+         u[1-iz][j][ycell] = u[iz][j][ycell] + CX*(u[iz][j+1][ycell] + u[iz][j-1][ycell] - 2.0*u[iz][j][ycell]) + CY*(u[iz][j][ycell+1] + u[iz][j][ycell-1] - 2.0*u[iz][j][ycell]);
       }
       
       /* Convergence check every INTERVAL iterations */
       #if CONVERGENCE
          if (i % INTERVAL == 0) {
             locdiff = 0.0;
-            #pragma omp for schedule(static,1) reduction(+:locdiff)
-            for (i = xs[my_rank]; i < xs[my_rank]+xcell; i++)
-               for (j = ys[my_rank]; j < ys[my_rank]+ycell; j++)
+            #pragma omp for schedule(static,1) collapse(2) reduction(+:locdiff)
+            for (i = 1; i < xcell+1; i++)
+               for (j = 1; j < ycell+1; j++)
                   locdiff += (u[iz][i][j] - u[1-iz][i][j])*(u[iz][i][j] - u[1-iz][i][j]); // square distance
-                        
             #pragma omp master
             {
                MPI_Allreduce(&locdiff, &totdiff, 1, MPI_FLOAT, MPI_SUM, comm2d);
@@ -240,12 +242,14 @@ int main(void) {
             if (tobreak) break;
          }
       #endif
+      
       #pragma omp master
       {
          iz = 1-iz; // swap arrays
-         MPI_Waitall(4, sendRequest, status); //wait to send everything
+         MPI_Waitall(4, sendRequest, MPI_STATUSES_IGNORE); //wait to send everything
       }
    }
+
    end_time = MPI_Wtime();
    
    local_elapsed_time = end_time - start_time;
