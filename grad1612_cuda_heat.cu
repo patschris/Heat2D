3f2b0f0af92a2a7506918db152aa3b6b55d8d6d3
Christos Patsouras <32415524+patschris@users.noreply.github.com>
2019-05-22 16:40:47 +0300

    Cuda implementation cudaMallocPitch to make u 2D

--- grad1612_cuda_heat.cu
+++ grad1612_cuda_heat.cu
@@ -5,32 +5,55 @@
 
 #define NXPROB 10                       /* x dimension of problem grid */
 #define NYPROB 10                       /* y dimension of problem grid */
-#define STEPS 100                       /* number of time steps */
+#define STEPS 1//00                       /* number of time steps */
 #define CX 0.1                          /* Old struct parms */
 #define CY 0.1
 #define MAXTHREADS 32                   /* Threads per block gpu */
-#define DEBUG  0                        /* Some extra messages  1: On, 0: Off */
+#define DEBUG  0                       /* Some extra messages  1: On, 0: Off */
 
 
- /******************* Prototypes *******************/
- __global__ void inidat(float *);
- __global__ void update(const float *, float *);
- __global__ void print (float *);
-/**************************************************/
+/* Array initialization */
+__global__ void inidat(float  *u, int iz, int pitch) {
+	const int ix = blockIdx.x * blockDim.x + threadIdx.x;
+    const int iy = blockIdx.y * blockDim.y + threadIdx.y;
+    if (ix>=0 && ix<NXPROB && iy>=0 && iy<NYPROB)
+        u[iz*pitch + ix*NYPROB+iy] = /*(ix*NYPROB+iy)*(iz+1);*/ (float)(ix * (NXPROB - ix - 1) * iy * (NYPROB - iy - 1));
+}
+
+__global__ void print (float *u, int iz, int pitch) {
+    for (int i =0; i<NXPROB; i++) {
+        for (int j=0; j<NYPROB; j++)
+            printf("%.2f ", u[iz*pitch + i*NYPROB+j]);
+        printf("\n");
+    }
+}
+
+
+/* Array update */
+__global__ void update(float *u, int iz, int pitch){
+
+	const int ix = blockIdx.x * blockDim.x + threadIdx.x ;
+	const int iy = blockIdx.y * blockDim.y + threadIdx.y ;
+
+    if (ix>0 && ix<NXPROB-1 && iy>0 && iy<NYPROB-1)
+        u[(1-iz)*pitch + ix*NYPROB + iy] = u[iz*pitch + ix*NYPROB + iy]  + 
+                CX* (u[iz*pitch + (ix+1)*NYPROB + iy] + u[iz*pitch + (ix-1)*NYPROB + iy] - 2.0 * u[iz*pitch + ix*NYPROB + iy]) + 
+                CY * (u[iz*pitch + ix*NYPROB + iy+1] + u[iz*pitch + ix*NYPROB + iy-1] - 2.0 * u[iz*pitch + ix*NYPROB + iy]);
+    
+}
 
 
 int main (void) {
-    float *u0, *u1;
+    float *u;
+    int iz, k;
+    size_t pitch;
     cudaError_t err;
 
-    if ((err = cudaMalloc((void**)&u0,  NXPROB * NYPROB * sizeof(float))) != cudaSuccess) {
-        printf("cudaMalloc failed u0: %s\n", cudaGetErrorString(err));
+   /* http://horacio9573.no-ip.org/cuda/group__CUDART__MEMORY_g80d689bc903792f906e49be4a0b6d8db.html */
+    if ((err = cudaMallocPitch((void**)&u, &pitch, NXPROB * NYPROB * sizeof(float), 2))!= cudaSuccess) {
+        printf("cudaMallocPitch failed u: %s\n", cudaGetErrorString(err));
         exit(-1);
-    }
-    if ((err = cudaMalloc((void**)&u1,  NXPROB * NYPROB * sizeof(float))) != cudaSuccess) {
-        printf("cudaMalloc failed u0: %s\n", cudaGetErrorString(err));
-        exit(-1);
-    }
+    } 
 
     #if DEBUG
         int devCount;
@@ -50,46 +73,32 @@ int main (void) {
 
     dim3 dimBlock((NXPROB+MAXTHREADS-1)/MAXTHREADS, (NXPROB+MAXTHREADS-1)/MAXTHREADS);
 	dim3 dimGrid (MAXTHREADS , MAXTHREADS);
-    inidat<<<dimBlock,dimGrid>>> (u0);
-    print<<<1,1>>> (u0);
+    inidat<<<dimBlock,dimGrid>>> (u, 0, pitch);
+    // inidat<<<dimBlock,dimGrid>>> (u, 1, pitch);
+    printf("Initial\nu0\n");
+    print<<<1,1>>>(u,0,pitch);
+    cudaDeviceSynchronize();
+    printf("\n\nu1\n");
+    print<<<1,1>>>(u,1,pitch); 
+    cudaDeviceSynchronize();
+    printf("----------------------------------------------------------------\n");
+    iz = 0;
+    for (k = 0; k < STEPS; k++) {
+        update<<<dimBlock,dimGrid>>> (u, iz, pitch);
+        cudaDeviceSynchronize();
+        iz = 1-iz;
+    }
+    printf("Final\nu0\n");
+    print<<<1,1>>>(u,0,pitch);
+    cudaDeviceSynchronize();
+    printf("\n\nu1\n");
+    print<<<1,1>>>(u,1,pitch); 
+    cudaDeviceSynchronize();
 
-    if (u0 && (err = cudaFree(u0)) != cudaSuccess) 
-        printf("cudaFree failed u0: %s\n", cudaGetErrorString(err));
-    if (u1 && (err = cudaFree(u1)) != cudaSuccess) 
-        printf("cudaFree failed u0: %s\n", cudaGetErrorString(err));
+    if (u && (err = cudaFree(u)) != cudaSuccess) 
+        printf("cudaFree failed u: %s\n", cudaGetErrorString(err));
     if (cudaDeviceReset() != cudaSuccess) 
         printf("cudaDeviceReset failed: %s\n", cudaGetErrorString(err));
 
     return 0;
-}
-
-/* Array initialization */
-__global__ void inidat(float  *u) {
-	const int ix = blockIdx.x * blockDim.x + threadIdx.x;
-    const int iy = blockIdx.y * blockDim.y + threadIdx.y;
-    if (ix>=0 && ix<NXPROB && iy>=0 && iy<NYPROB)
-        u[ix*NYPROB+iy] = (float)(ix * (NXPROB - ix - 1) * iy * (NYPROB - iy - 1));
-}
-
-__global__ void print (float *u) {
-    for (int i =0; i<NXPROB; i++) {
-        for (int j=0; j<NYPROB; j++)
-            printf("%.2f ", u[i*NYPROB+j]);
-        printf("\n");
-    }
-}
-
-
-/* Array update */
-__global__ void update(const float *init, float *dest){
-
-	const int ix = blockIdx.x * blockDim.x + threadIdx.x ;
-	const int iy = blockIdx.y * blockDim.y + threadIdx.y ;
-	
-    if (ix>0 && ix<NXPROB-1 && iy>0 && iy<NYPROB-1) {
-        dest[ix*NYPROB+iy] = 
-            init[ix*NYPROB+iy]  + 
-            CX* (init[(ix+1)*NYPROB+iy] + init[(ix-1)*NYPROB+iy] - 2.0 * init[ix*NYPROB+iy]) +
-            CY * (init[ix*NYPROB+iy+1] + init[ix*NYPROB+iy-1] - 2.0 * init[ix*NYPROB+iy]);
-    }
 }
